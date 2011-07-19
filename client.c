ce2b061243dda8310800861062b5f2af611abb54
dataplayer <flores314@yahoo.com>
2011-07-19 12:26:32 -0600

    first try still buggy

---
+++ client.c
@@ -0,0 +1,105 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+// to do
+// chance PF_INET to PF_UNIX will have to change address
+// no more ip at this point...binding will be different too
+
+#define ERR_CONN_FAILED (2)
+#define PORT (5842)
+#define OTW_ADDR "69.55.233.89"
+
+void dump_hex(void *buffer, int len);
+
+struct four_ints {
+  unsigned int int1;
+  unsigned int int2;
+  unsigned int int3;
+  unsigned int int4;
+};
+
+
+int main(int argc, char *argv[])
+{
+  
+  int sock;
+  
+  printf("getting a sock\n");
+  
+  sock = socket(PF_INET,SOCK_STREAM,0);
+  
+  if (sock < 0)
+    exit(1);
+
+  int connection;
+  char *url = "vortex.labs.overthewire.org";
+  char buffer[256];
+  struct sockaddr_in *address = (struct sockaddr_in*) buffer;
+
+  printf("about to run inet_pton \n");
+  if( inet_pton(PF_INET, OTW_ADDR, &address[0].sin_addr) != 1 ) 
+  {
+	printf("inet_pton failed errno = %i \n", errno);
+	exit(3);
+  }
+
+  // populate arguments to connect function
+  address[0].sin_family = PF_INET;
+  address[0].sin_port = htons(PORT);
+  
+  printf("about to connect \n");
+  connection = connect(sock,(struct sockaddr*) address,sizeof(struct sockaddr_in));
+  printf("finished connecting \n");
+
+  if (connection != 0)
+  {
+    printf("connection failed with errno: %i \n", errno);
+    exit(ERR_CONN_FAILED);
+  }
+
+  // read ints transform and print
+  struct four_ints data;
+  int y;
+  y = read(sock,&data,sizeof(data));
+  printf("received ints: %u,%u,%u,%u \nbytes received: %i \n", data.int1,data.int2,data.int3,data.int4,y);
+  dump_hex(&data,sizeof(data));
+
+  // get ready to add up the ints and send
+  int *pdata = (int*) &data;
+  int index;
+  unsigned long long sum = 0;
+  for (index = 0; index < 4; index++)
+  {
+    sum = pdata[index] + sum;
+  }
+ 
+  // send the sum of the ints
+  dump_hex(&sum,sizeof(sum));
+  write(sock,&sum,sizeof(sum));
+
+  // read the message from the server
+  char info;
+  y = read(sock,&info,sizeof(info));
+  while ( y > 0 )
+  {
+    printf("%c\n",info);
+    y = read(sock,&info,sizeof(info));
+  }
+
+  return 0;
+}
+
+
+void dump_hex(void * buffer, int len) {
+    unsigned char * p = (unsigned char *) buffer;
+    int i;
+    for(i=0;i<len;i++) 
+        printf("%02X ", p[i]);
+    printf("\n");
+}
